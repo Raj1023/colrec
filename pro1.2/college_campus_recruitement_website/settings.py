c0ce784e6b82045f694fa8ece86066f8eaf4e072
Raj1023 <rajbkhaire10@gmail.com>
2026-04-13 14:43:48 +0530

    Update settings.py

--- pro1.2/college_campus_recruitement_website/settings.py
+++ pro1.2/college_campus_recruitement_website/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3xwu$6z6&x6c^d6ns5t0qmdono1&r1s@@pn5@by@@n=n24dtr1'
+SECRET_KEY = 'os.environment.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
 
 # Application definition
